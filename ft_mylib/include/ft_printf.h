cce60a2eb5d720f3f1bb7759ca8f72466e5a592b
diegonmarcos <diegonmarcos@gmail.com>
2025-01-24 08:46:12 +0100

    up

--- ft_mylib/include/ft_printf.h
+++ ft_mylib/include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: dinepomu <dinepomu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 12:32:10 by dinepomu          #+#    #+#             */
-/*   Updated: 2025/01/22 15:25:21 by dinepomu         ###   ########.fr       */
+/*   Updated: 2025/01/24 08:44:21 by dinepomu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,6 @@ int				ft_printf(const	char *str, ...);
  * function.
  */
 
-int	ft_fprintf(char *filename, const char *str, ...);
+int				ft_fprintf(char *filename, const char *str, ...);
 
 #endif
