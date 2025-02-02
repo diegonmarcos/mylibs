4a0c8aa31dcd401ee22d5ec8093045c4bfc61d0e
diegonmarcos <diegonmarcos@gmail.com>
2025-02-02 15:59:23 +0100

    ALLEESS GUTT

--- ft_mylib/include/ft_mylib.h
+++ ft_mylib/include/ft_mylib.h
@@ -6,7 +6,7 @@
 /*   By: dinepomu <dinepomu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 14:47:54 by dnepomuc          #+#    #+#             */
-/*   Updated: 2025/02/01 18:33:41 by dinepomu         ###   ########.fr       */
+/*   Updated: 2025/02/02 15:58:46 by dinepomu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 # define FT_MYLIB_H
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓	//
 
-/*#########################################################################	*/
-/* 0.0 MACROS																*/
-/*#########################################################################	*/
+/*#####################################################################	*/
+/* 0.0 MACROS															*/
+/*#####################################################################	*/
 // GET NEXT LINE2
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1000
@@ -36,9 +36,9 @@
 #  define NAME_M "log/5.bff/malloc_allocs.txt"
 # endif
 
-/*#########################################################################	*/
-/* 0.1 LIBRARIES															*/
-/*#########################################################################	*/
+/*#####################################################################	*/
+/* 0.1 LIBRARIES														*/
+/*#####################################################################	*/
 # include <unistd.h>	// open, close, read, write, fork(), exec(), pipe()
 # include <fcntl.h>		// file control O_RDONLY, O_RDWR, O_APPEND, O_TRUNC
 # include <stdarg.h> 	// Variadic functions for fprintf and printf
@@ -50,45 +50,8 @@
 
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓	//
 
-/*#########################################################################	*/
-/* 1. MEMORY MANAGEMENT														*/
-/*#########################################################################	*/
-
 /* ***************************************************************** */
-/* ***************************************************************** */
-void	print_bits(unsigned char octet);
-/* ***************************************************************** */
-/* ***************************************************************** */
-void	*ft_calloc(size_t count, size_t size);
-void	halt_exit_(int status);
-void	halt_exit1(int status);
-void	halt_exit(int status, char *description);
-
-/* ***************************************************************** */
-/* ***************************************************************** */
-void	*ft_calloc_fd(char *filename, size_t count, size_t sizeofvar);
-void	ft_free_fd(char *filename);
-void	ft_free_array_2d(char **array);
-void	ft_free_array_int(int *array_int, int size);
-void	ft_free_fd_new(char *filename);
-
-/* ***************************************************************** */
-/* ***************************************************************** */
-typedef struct s_list_garbage
-{
-	void	*allocated_pointers[MAX_ALLOCATIONS];
-	int		blocks_count;
-}			t_list_garbage;
-
-void	ft_initializator_list_gargabe(t_list_garbage *my_list);
-void	*ft_malloc_gb(size_t size, t_list_garbage *list_garbage);
-int		add_allocation(void *ptr, t_list_garbage *list_garbage);
-int		is_allocated(void *ptr, t_list_garbage *list_garbage);
-void	remove_allocation(void *ptr, t_list_garbage *list_garbage);
-void	ft_free_gb(void *ptr, t_list_garbage *list_garbage);
-void	ft_free_gb_all(t_list_garbage *list_garbage);
-
-/* ***************************************************************** */
+/* 1. STRING MANIPULATION low leve									 */
 /* ***************************************************************** */
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
@@ -99,114 +62,86 @@ void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 
 /*#########################################################################	*/
-/* 2. STRING MANIPULATION 													*/
+/* 2. STRING MANIPULATION high level										*/
 /*#########################################################################	*/
 
-/* ***************************************************************** */
-/* ***************************************************************** */
+/* ************************************************************* */
+/* ************************************************************* */
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strdup(char *name, const char *s1);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_strcmp(const char *s1, const char *s2);
-size_t	ft_strlen(const char *str);
-
-/* ***************************************************************** */
-/* ***************************************************************** */
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strjoin(char *filename, char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char *filename, char const *s, unsigned int start,
 			size_t len);
 char	**ft_split(char *filename, const char *s, char c);
-void	ft_free_array_halt(char **farray, int failed);
 int		*ft_split_int(char *filename, char const *array_source,
 			char delimitator, int *size);
 int		ft_contains(int num, char **argv, int i);
 
-/* ***************************************************************** */
-/* ***************************************************************** */
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-char	*ft_strdup(char *name, const char *s1);
-
-/* ***************************************************************** */
-/* ***************************************************************** */
-char	*ft_strrchr(const char *s, int c);
+/* ************************************************************* */
+/* ************************************************************* */
 char	*ft_strchr(const char *s, int c);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strrchr(const char *s, int c);
 int		ft_strchr_char_duplicate(int num, char **argv, int i);
 int		*ft_strchr_int_duplicate(int *array_int, int size, int position);
 int		*ft_strchr_int_duplicate_interation(int *array_int, int size);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
-/* ***************************************************************** */
-/* ***************************************************************** */
+/* ************************************************************* */
+/* SRING MANIPULATION / CONVERTERS AND CHECKERS					 */
+/* ************************************************************* */
+long	ft_atoi(const char *str);
+void	*ft_atoi_base(const char *str, int str_base);
+int		ft_atoi_safe(const char *ptr);
+int		*ft_atoi_safe_array_2d(const char **ptr);
+char	*ft_itoa(int n);
+
+/* ************************************************************* */
+/* ************************************************************* */
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isnum(const char *num);
 int		ft_isprint(int c);
-long	ft_atoi(const char *str);
-void	*ft_atoi_base(const char *str, int str_base);
-int		ft_atoi_safe(const char *ptr);
-int		*ft_atoi_safe_array_2d(const char **ptr);
-char	*ft_itoa(int n);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+
+/* ************************************************************* */
+/* STRING MANIP / HALTERS										 */
+/* ************************************************************* */
+void	halt_exit_(int status);
+void	halt_exit1(int status);
+void	halt_exit(int status, char *description);
 
 /*#########################################################################	*/
 /* 3. STANDARDT/STDERROR OUTPUT AND FILE DESCRIPTORS MANIP (IO)				*/
 /*#########################################################################	*/
 
-/* ***************************************************************** */
-/* ***************************************************************** */
+/* ************************************************************* */
+/* PRINTERS low level											 */
+/* ************************************************************* */
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char *s, int fd);
 int		ft_putnbr(int n);
 
-/* ***************************************************************** */
-/* ***************************************************************** */
-//FT_FPRINTF
+/* ************************************************************* */
+/* PRINTERS high level											 */
+/* ************************************************************* */
+int		ft_fprintf(char *filename, const char *str, ...);
 int		ft_fprintf1(char *filename, const char *str, ...);
-
-//
-int		ft_putstr_fd_fpr(long fd, char *s);
-int		ft_putnbr_fd_fpr(long fd, int n);
-int		ft_putchar_fd_fpr(long fd, int c);
-int		ft_putchar(char c);
-int		ft_putchar_int(int c);
-int		ft_putstr(char *str);
-int		ft_pointer_hexa_fpr(long fd, void *p);
-
-/* ***************************************************************** */
-/* ***************************************************************** */
-//FT_PRINTF
 int		ft_printf(const	char *str, ...);
-size_t	ft_num_unsign(size_t num);
-int		ft_pointer_hexa(void *p);
-int		ft_hexa_minusc(int num);
-int		ft_hexa_mayusc(int num);
 
-/* ***************************************************************** */
-/* ***************************************************************** */
-//GET_NEXT_LINE2
-int		get_next_line2(int fd, char **line);
-//
-size_t	ft_strlen_gnl(char *s, int c);
-char	*ft_strchr_gnl(char *s, int c);
-char	*ft_strjoin_gnl(char *left_str, char *buff, int c);
-int		ft_read_to_left_str2(int fd, char **left_str);
-char	*ft_new_left_str(char *left_str);
-char	*ft_get_line(char *left_str);
-
-/* ***************************************************************** */
-/* ***************************************************************** */
-//GET_NEXT_LINE
+/* ************************************************************* */
+/* ************************************************************* */
 char	*get_next_line(int fd);
-char	*ft_new_left_str(char *left_str);
-char	*ft_get_line(char *left_str);
-
-/* ***************************************************************** */
-/* ***************************************************************** */
+int		get_next_line2(int fd, char **line);
 // FGETS(gnl) | FGETS2(gnl2) | FSCANF(ft_split)
 int		ft_fgets(char **line, int fd);
 void	ft_scanf(char *filename, char *line, void **pointers,
@@ -216,13 +151,15 @@ void	ft_scanf(char *filename, char *line, void **pointers,
 /* 4. ALGOS																	*/
 /*#########################################################################	*/
 
-/* ***************************************************************** */
-/* ***************************************************************** */
+/* ************************************************************* */
+/* ************************************************************* */
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	print_bits(unsigned char octet);
 
-/* ***************************************************************** */
-/* ***************************************************************** */
+/* ************************************************************* */
+/* ALGOS/SORTING 												 */
+/* ************************************************************* */
 void	heapify(int arr[], int n, int i);
 void	heap_sort(int arr[], int n);
 void	quick_sort(int arr[], int low, int high, int depthlimit);
@@ -232,14 +169,25 @@ void	swap_introsort(int *a, int *b);
 int		partition(int arr[], int low, int high);
 void	heapify(int arr[], int n, int i);
 
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓	//
 /*#########################################################################	*/
-/* 5. DATA STRUCTURES														*/
+/* 5. DATA STRUCTURES & MEMORY MANAGEMENT									*/
 /*#########################################################################	*/
 
-/* ***************************************************************** */
-/* ***************************************************************** */
-// DATA STRUCTURE FOR HASHTABLE/MAP/DIC
+/* ************************************************************* */
+/* DATA STRUCTURE / ARRAYS										 */
+/* ************************************************************* */
+void	*ft_calloc(size_t count, size_t size);
+void	*ft_calloc_fd(char *filename, size_t count, size_t sizeofvar);
+void	ft_free_fd(char *filename);
+void	ft_free_array_2d(char **array);
+void	ft_free_array_int(int *array_int, int size);
+void	ft_free_fd_new(char *filename);
+void	ft_free_array_halt(char **farray, int failed);
 
+/* ************************************************************* */
+/* DATA STRUCTURE / HASHTABLE/MAP/DIC							 */
+/* ************************************************************* */
 typedef struct s_node_
 {
 	char			*key;
@@ -253,9 +201,8 @@ void	free_hashtable(t_node_ **table, int size);
 size_t	hash(char *key, int size);
 t_node_	*create_node(char *key, int value);
 
-/* ***************************************************************** */
-/* ***************************************************************** */
-
+/* ************************************************************* */
+/* ************************************************************* */
 // DATA STRUCTURE FOR SINGLY LINEKD LIST
 
 typedef struct s_list_s
@@ -277,8 +224,8 @@ void	ft_lstiter(t_list_s *lst, void (*f)(void*));
 t_list_s	*ft_lstmap(t_list_s *lst, void *(*f)(void*), void (*del)(void*));
 */
 
-/* ***************************************************************** */
-/* ***************************************************************** */
+/* ************************************************************* */
+/* ************************************************************* */
 // DATA STRUCTURE FOR DOUBLY LINKED LIST
 
 typedef struct s_list
@@ -289,18 +236,33 @@ typedef struct s_list
 	struct s_list	*prev;
 }				t_list;
 
+t_list	*array_to_d_linked_list(char **argv);
+t_list	*array_int_to_d_linked_list(int *argv, int size);
 t_list	*ft_lstnew(int value, int index);
 void	ft_lstadd_back(t_list **stack, t_list *new_node);
 int		ft_lstsize(t_list *lst);
 t_list	*find_min_node(t_list *stack);
 void	ft_swap_pointers(t_list **a, t_list **b);
-t_list	*array_to_d_linked_list(char **argv);
-t_list	*array_int_to_d_linked_list(int *argv, int size);
 void	ft_print_ls_doubly(t_list *stack);
 void	ft_free_ls_doubly(t_list *stack);
 
-/* ***************************************************************** */
-/* ***************************************************************** */
+/* ************************************************************* */
+/* MEMORY MANAGEMENT / GARBAGE COLLECTOR						 */
+/* ************************************************************* */
+typedef struct s_list_garbage
+{
+	void	*allocated_pointers[MAX_ALLOCATIONS];
+	int		blocks_count;
+}			t_list_garbage;
+
+void	ft_initializator_list_gargabe(t_list_garbage *my_list);
+void	*ft_malloc_gb(size_t size, t_list_garbage *list_garbage);
+int		add_allocation(void *ptr, t_list_garbage *list_garbage);
+int		is_allocated(void *ptr, t_list_garbage *list_garbage);
+void	remove_allocation(void *ptr, t_list_garbage *list_garbage);
+void	ft_free_gb(void *ptr, t_list_garbage *list_garbage);
+void	ft_free_gb_all(t_list_garbage *list_garbage);
+
 // FOR THE PRINTERS DEBUGGERS
 /*
 typedef struct s_node
@@ -309,5 +271,40 @@ typedef struct s_node
 	struct s_node	*next;
 }				t_node;
 */
+
+/* ************************************************************* */
+/* ************************************************************* */
+//FT_FPRINTF HELPERS
+int		ft_putstr_fd_fpr(long fd, char *s);
+int		ft_putnbr_fd_fpr(long fd, int n);
+int		ft_putchar_fd_fpr(long fd, int c);
+int		ft_putchar(char c);
+int		ft_putchar_int(int c);
+int		ft_putstr(char *str);
+int		ft_pointer_hexa_fpr(long fd, void *p);
+
+/* ************************************************************* */
+/* ************************************************************* */
+//FT_PRINTF HELPERS
+size_t	ft_num_unsign(size_t num);
+int		ft_pointer_hexa(void *p);
+int		ft_hexa_minusc(int num);
+int		ft_hexa_mayusc(int num);
+
+/* ************************************************************* */
+/* ************************************************************* */
+//GET_NEXT_LINE2 HELPERS
+size_t	ft_strlen_gnl(char *s, int c);
+char	*ft_strchr_gnl(char *s, int c);
+char	*ft_strjoin_gnl(char *left_str, char *buff, int c);
+int		ft_read_to_left_str2(int fd, char **left_str);
+char	*ft_new_left_str(char *left_str);
+char	*ft_get_line(char *left_str);
+
+/* ************************************************************* */
+/* ************************************************************* */
+//GET_NEXT_LINE HELPERS
+char	*ft_new_left_str(char *left_str);
+char	*ft_get_line(char *left_str);
 
 #endif
