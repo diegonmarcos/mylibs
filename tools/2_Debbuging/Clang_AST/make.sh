2ac6e87df03e78b4081b54d0d47dc0f906de23bd
diegonmarcos <diegonmarcos@gmail.com>
2025-02-09 17:54:50 +0100

    clang_ast

--- tools/2_Debbuging/Clang_AST/make.sh
+++ tools/2_Debbuging/Clang_AST/make.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 set -v
-exec > ast.log 2>&1
-set > env_vars.txt
+set > ast.log
+exec >> ast.log 2>&1
+
+# Check if llvm-config is available
+if ! command -v llvm-config &> /dev/null
+then
+	if [ -d "/usr/lib/llvm-18/bin" ]; then
+  	export PATH="/usr/lib/llvm-18/bin:$PATH"
+	fi
+	if ! command -v llvm-config &> /dev/null
+	then
+		echo "llvm-config could not be found"
+		exit 1
+	fi
+fi
+# Check if clang++ is available
+if ! command -v clang++ &> /dev/null
+then
+	echo "clang++ could not be found"
+	exit 1
+fi
+
 
 
 # Get C++ flags.
@@ -13,10 +33,10 @@ LINCLUDE="-I$(llvm-config --includedir)"
 # Get linker flags and libraries *in the correct order*.  This is the crucial part.
 LDFLAGS=$(llvm-config --ldflags)
 COMP_LIBS="-lclangTooling -lclangFrontend -lclangAST -lclangParse -lclangSema -lclangAnalysis -lclangEdit -lclangLex -lclangBasic -lclangDriver -lclangCodeGen"
-LIBS=$(llvm-config --libs) $(llvm-config --system-libs)
+LIBS="$(llvm-config --libs) $(llvm-config --system-libs)"
 
 # Build the command line.
-clang++ $CXXFLAGS $LINCLUDE -o clang_ast ast.cpp $LDFLAGS $COMP_LIBS $LIBS > output_file.txt
+clang++ $CXXFLAGS $LINCLUDE -o clang_ast ast.cpp $LDFLAGS $COMP_LIBS $LIBS
 
 echo "CXXFLAGS: $CXXFLAGS"
 echo "LINCLUDE: $LINCLUDE"
