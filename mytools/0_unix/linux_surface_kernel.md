9d327ad8b06cae650ce0dffca30d642610e3f47c
diegonmarcos <diegonmarcos@gmail.com>
2025-03-02 13:38:09 +0100

    up

---
+++ mytools/0_unix/linux_surface_kernel.md
@@ -0,0 +1,7 @@
+# LINUX SURFACE KERNEL AND LIBRARIES
+
+# GIT
+https://github.com/linux-surface/linux-surface?tab=readme-ov-file  
+
+# INSTALL
+https://github.com/linux-surface/linux-surface/wiki/Installation-and-Setup
---
+++ mytools/0_unix/packages_list/dependec_list.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#apt list --installed | awk '/\/installed$/ { print $1 }'
+apt list --installed
+apt list --installed  > installed_packages_apt.txt
+
+pip list
+pip freeze > installed_packages_pip.txt
+
+snap list
+snap list > installed_packages_snap.txt
+
+flatpak list
+flatpak list > installed_packages_flatpak.txt
